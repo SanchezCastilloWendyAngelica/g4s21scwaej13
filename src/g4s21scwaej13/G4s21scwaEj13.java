0e58cd725aa10c106d231a041f6eaf26e63a9a8e
SanchezCastilloWendyAngelica <sanchezwendy854@gmail.com>
2023-05-12 11:27:15 -0600

    proyecto tarea

---
+++ src/g4s21scwaej13/G4s21scwaEj13.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21scwaej13;
+
+/**
+ *
+ * @author hp
+ */
+public class G4s21scwaEj13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFPrincipal jFPrincipal = new JFPrincipal();
+        new JFPrincipal .setVisible(true);
+        
+        
+            
+    }
+    
+}
